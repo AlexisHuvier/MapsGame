edb99f43554f357e72de3fa012c75371566aaa49
LavaPower <lavapower84@gmail.com>
2019-04-08 14:51:25 +0200

    Create Editor

--- Core/Game.py
+++ Core/Game.py
@@ -1,11 +1,15 @@
 import pygame
 from pygame import locals as const
-from tkinter import Tk, Label, Button, StringVar, OptionMenu
+from tkinter import Tk, Label, Button, StringVar, OptionMenu, Entry
+from tkinter.messagebox import askyesno
 import os
+import json
 
 from Core.Map import Map
 from Core.Block import ListBlockTypes
 from Core.Player import Player
+
+from MapEditor.Editor import Editor
 
 
 class Game:
@@ -131,20 +135,36 @@ class Game:
 
 class MainMenu:
     def __init__(self):
+        self.namefen = None  # Respect PEP8
+        self.nameentry = None  # Respect PEP8
+
         self.fenetre = Tk()
         self.fenetre.title("MapsGame")
+        self.fenetre.resizable(False, False)
+        self.fenetre.geometry("200x380")
 
-        titre = Label(self.fenetre, text="MapsGame")
+        self.fonts = {
+            "title": ("Arial", 20, 'bold'),
+            "namefen": ("Arial", 12),
+            "other": ("Arial", 14)
+        }
+
+        titre = Label(self.fenetre, text="MapsGame", font=self.fonts["title"])
         listeoptions = os.listdir("maps")
         self.v = StringVar()
         self.v.set(listeoptions[0])
         om = OptionMenu(self.fenetre, self.v, *listeoptions)
-        launchbutton = Button(self.fenetre, text="Jouer", command=self.launchgame)
-        exitbutton = Button(self.fenetre, text="Quitter", command=self.fenetre.destroy)
+        om['font'] = self.fonts["other"]
+        launchbutton = Button(self.fenetre, text="Jouer", font=self.fonts["other"], command=self.launchgame)
+        editbutton = Button(self.fenetre, text="Editer Map", font=self.fonts["other"], command=self.editmap)
+        createbutton = Button(self.fenetre, text="Créer Map", font=self.fonts["other"], command=self.createmap)
+        exitbutton = Button(self.fenetre, text="Quitter", font=self.fonts["other"], command=self.fenetre.destroy)
 
-        titre.pack(padx=10, pady=15)
+        titre.pack(padx=10, pady=20)
         om.pack(padx=10, pady=10)
         launchbutton.pack(padx=10, pady=5)
+        editbutton.pack(padx=10, pady=10)
+        createbutton.pack(padx=10, pady=5)
         exitbutton.pack(padx=10, pady=10)
 
         self.fenetre.mainloop()
@@ -153,3 +173,58 @@ class MainMenu:
         self.fenetre.destroy()
 
         Game(self.v.get())
+
+    def editmap(self):
+        self.fenetre.destroy()
+
+        Editor(self.v.get())
+
+    def createmap(self):
+        self.fenetre.destroy()
+        self.namefen = Tk()
+
+        namelabel = Label(self.namefen, text="Nom de la map", font=self.fonts["namefen"])
+        self.nameentry = Entry(self.namefen)
+        entrybutton = Button(self.namefen, text="Valider", font=self.fonts["namefen"], command=self.validatecreation)
+        cancelbutton = Button(self.namefen, text="Annuler", font=self.fonts["namefen"], command=self.cancelcreation)
+
+        namelabel.grid(row=1, column=1, columnspan=2, padx=20, pady=10)
+        self.nameentry.grid(row=2, column=1, columnspan=2, padx=20, pady=0)
+        entrybutton.grid(row=3, column=1, padx=10, pady=10)
+        cancelbutton.grid(row=3, column=2, padx=10, pady=10)
+
+        self.namefen.mainloop()
+
+    def validatecreation(self):
+
+        mapdir = self.nameentry.get()
+        if mapdir in os.listdir("maps"):
+            if askyesno("Maps Déjà Existante", "La map existe déjà.\nVoulez-vous l'éditer ?"):
+                self.namefen.destroy()
+                Editor(mapdir)
+        else:
+            self.namefen.destroy()
+            os.mkdir("maps/"+mapdir)
+            blocksjson = {
+                "types": []
+            }
+            mapjson = {
+                "name": "Nom",
+                "description": "Description",
+                "author": "Auteur",
+                "scoreToWin": -1,
+                "looseOnFall": False,
+                "blocks": []
+            }
+            with open("maps/"+mapdir+'/map.json', 'w') as f:
+                f.write(json.dumps(mapjson, indent=4))
+            with open("maps/"+mapdir+'/blocks.json', 'w') as f:
+                f.write(json.dumps(blocksjson, indent=4))
+            Editor(mapdir)
+
+    def cancelcreation(self):
+        self.namefen.destroy()
+        self.__init__()
+
+
+
---
+++ MapEditor/Editor.py
@@ -0,0 +1,43 @@
+from tkinter import Tk, Label, Button
+
+
+
+class Editor:
+    def __init__(self, mapdir):
+        self.mapdir = mapdir
+        self.fenetre = Tk()
+        self.fenetre.title("MapEditor : "+mapdir)
+        self.fenetre.resizable(False, False)
+        self.fenetre.geometry("300x375")
+
+        fonts = {
+            "title": ("Arial", 20, 'bold'),
+            "other": ("Arial", 14)
+        }
+
+        titre = Label(self.fenetre, text="Map : "+mapdir, font=fonts["title"])
+        blockstypesbutton = Button(self.fenetre, text="Editer les BlockTypes", font=fonts["other"],
+                                   command=self.editblockstypes)
+        mapinfosbutton = Button(self.fenetre, text="Editer les MapInfos", font=fonts["other"],
+                                command=self.fenetre.destroy)
+        blocksbutton = Button(self.fenetre, text="Editer les Blocks", font=fonts["other"], command=self.fenetre.destroy)
+        retourbutton = Button(self.fenetre, text="Retour Menu Principal", font=fonts["other"], command=self.mainmenu)
+        quitterbutton = Button(self.fenetre, text="Quitter", font=fonts["other"], command=self.fenetre.destroy)
+
+        titre.pack(padx=10, pady=20)
+        blockstypesbutton.pack(padx=10, pady=10)
+        mapinfosbutton.pack(padx=10, pady=5)
+        blocksbutton.pack(padx=10, pady=10)
+        retourbutton.pack(padx=10, pady=5)
+        quitterbutton.pack(padx=10, pady=10)
+
+        self.fenetre.mainloop()
+
+    def editblockstypes(self):
+        self.fenetre.destroy()
+
+    def mainmenu(self):
+        from Core.Game import MainMenu
+
+        self.fenetre.destroy()
+        MainMenu()
